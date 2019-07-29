91ea7a78f65f10c77fe460b8630daae398d35e28
liujialin <976247042@qq.com>
2019-07-29 11:39:07 +0800

    springboot性能优化之扫包优化

--- src/main/java/com/ljl/FirstSpringBootApplication.java
+++ src/main/java/com/ljl/FirstSpringBootApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/*
+*  1.扫包优化 使用定位扫包的方式
+*  EnableAutoConfiguration
+*  ComponentScan({''})
+* */
 @SpringBootApplication
 //该注解默认等同于EnableAutoConfiguration + ComponentScan(默认为扫描该文件所在包以及他的所有子包)
 //所以需要将他放到跟包下才可以扫描到所以的注解
